18a6e41c4460d14d50a9375b9c9aa16a06b37a7f
Lee Jinseop <js428118@gmail.com>
2020-07-17 18:49:30 +0900

    clean

--- api/post.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app, models) => {
-    app.get('/posts', (req, res) => {
-        models.Posts.findAll().then((result) => res.json(result))
-    })
-}
--- api/user.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app, models) => {
-    app.get('/users', (req, res) => {
-        models.Users.findAll().then((result) => res.json(result))
-    })
-}
